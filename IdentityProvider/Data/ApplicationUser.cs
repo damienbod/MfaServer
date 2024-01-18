72b18f0b711d31053472cea57eb05cbe60ef7da6
damienbod <damien_bod@hotmail.com>
2024-01-18 09:09:43 +0100

    Update data

--- IdentityProvider/Data/ApplicationUser.cs
+++ IdentityProvider/Data/ApplicationUser.cs
@@ -2,5 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace OpeniddictServer.Data;
 
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser { }
+public class ApplicationUser : IdentityUser 
+{ 
+    public string EntraIdOid { get; set; }
+}
