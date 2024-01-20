7b385f1dee7d1a2f0f338f030723f0d6a9cb9f83
damienbod <damien_bod@hotmail.com>
2024-01-20 08:58:51 +0100

    remove code that does not work

--- TestApplications/PoraApp/MicrosoftGraphClient.cs
+++ TestApplications/PoraApp/MicrosoftGraphClient.cs
@@ -10,52 +10,18 @@ namespace MerillApp;
 
 public class MicrosoftGraphClient
 {
-    private readonly GraphServiceClient _graphServiceClient;
     private readonly IHttpClientFactory _clientFactory;
     private readonly ITokenAcquisition _tokenAcquisition;
     private readonly IConfiguration _configuration;
 
-    public MicrosoftGraphClient(GraphServiceClient graphServiceClient, 
-        IHttpClientFactory clientFactory,
+    public MicrosoftGraphClient(IHttpClientFactory clientFactory,
        ITokenAcquisition tokenAcquisition,
        IConfiguration configuration)
     {
-        _graphServiceClient = graphServiceClient;
         _clientFactory = clientFactory;
         _tokenAcquisition = tokenAcquisition;
         _configuration = configuration;
     }
-
-    /// <summary>
-    ///https://graph.microsoft.com/beta/policies/authenticationMethodsPolicy/authenticationMethodConfigurations
-    ///{
-    ///  "@odata.type": "#microsoft.graph.externalAuthenticationMethodConfiguration",
-    ///  "displayName": "ILoveAuthN",
-    ///  "appId": "600b719b-3766-4dc5-95a6-3c4a8dc31885",
-    ///  "openIdConnectSetting": {
-    ///    "clientId": "06a011bd-ec92-4404-80fb-db6d5ada8ee2",
-    ///    "discoveryUrl": "https//iloveauthn.com/.well-known/openid-configuration"
-    ///  }
-    ///}
-    /// </summary>
-    //public async Task<AuthenticationMethodConfiguration?> CreateAuthenticationMethod()
-    //{
-    //    var oidcSetting = System.Text.Json.JsonSerializer.Serialize(new openIdConnectSetting());
-    //    var additionalData = new Dictionary<string, object>
-    //    {
-    //        { "displayName", "FIDO2-passkeys-MFA-b8dcfa58960c.ngrok.app" },
-    //        { "appId", "c5684f52-769e-471a-b7b5-9b9a94af97d4" },
-    //        { "openIdConnectSetting", oidcSetting }
-    //    };
-    //    var created = await _graphServiceClient.AuthenticationMethodsPolicy
-    //        .AuthenticationMethodConfigurations.PostAsync(new AuthenticationMethodConfiguration
-    //        {
-    //            AdditionalData = additionalData, 
-    //            OdataType = "#microsoft.graph.externalAuthenticationMethodConfiguration",
-    //        }, b => b.Options.WithScopes("Policy.ReadWrite.AuthenticationMethod"));
-
-    //    return created;
-    //}
 
     /// <summary>
     ///https://graph.microsoft.com/beta/policies/authenticationMethodsPolicy/authenticationMethodConfigurations
